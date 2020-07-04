e65361a1cbba3633ebabe8df8f1bd0fd7a29aba3
arhunt <allan.r.hunt@gmail.com>
2020-07-04 08:21:56 -0500

    Revised output formatting.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,10 +19,10 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
+        # Processing each row as a vote
     for row in csvreader:
             # Count the number of votes
         votes_counted = votes_counted + 1
-
             # Is the candidate already on the tally?
         if row[2] not in votes:
                 # No, add them in with one vote
@@ -31,12 +31,12 @@ with open(csvpath) as csvfile:
                 # Yes, add one to their vote count
             votes[row[2]] += 1
 
-    # Create a list with the final vote values        
+    # Create a list with the final vote values
 vote_values = list(votes.values())
 
     # Once all the calculations are done, figure percentages
 for candidate in votes:
-        # Matching the rounded decimals
+        # Matching the rounded decimals in the example
     percent_vote = "{:.3%}".format(votes[candidate] / votes_counted)
         # Add the candidate's percentage to be able to reference
     percentage[candidate] = percent_vote
@@ -53,16 +53,17 @@ print("-------------------------")
 for candidate in votes:
     print(f"{candidate}: {percentage[candidate]} ({votes[candidate]})")
 print("-------------------------")
-
+    # Determine a single winner or a tie
 if len(winner) == 1:
     for candidate in winner:
         print(f"Winner: {candidate}")
 else:
-    print("There is a tie between:")
+    print("Winner: Tie between:")
     for candidate in winner:
         print(f"{candidate}")
 
 print("-------------------------")
+print("")
 
     # Provide the text file path
 txtpath = os.path.join('analysis', 'analysis.txt')
@@ -81,7 +82,7 @@ with open(txtpath, "w") as txtfile:
         for candidate in winner:
             txtfile.write(f"Winner: {candidate}\n")
     else:
-        txtfile.write("There is a tie between:\n")
+        txtfile.write("Winner: Tie between:\n")
         for candidate in winner:
             txtfile.write(f"{candidate}\n")
 
